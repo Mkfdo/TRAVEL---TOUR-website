4aa2c1594a894a62ca40bf6f368c77bd81505e52
Mkfdo <mithila201213@gmail.com>
2024-07-25 12:57:15 +0530

    Adding components to the App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/Home';
+import Main from './Components/Main/Main';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
-    <h2>App</h2>
+    <>
+    <Navbar />
+    <Home />
+    <Main />
+    <Footer />
+    </>
   );
 }
 
