40f56f9b0b97612dfa741c9ff8653dd2292bedb5
Mkfdo <mithila201213@gmail.com>
2024-08-01 12:33:50 +0530

    WIP navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './navbar.css'
 import {MdOutlineTravelExplore} from 'react-icons/md'
 import {AiFillCloseCircle} from 'react-icons/ai'
 import {TbGridDots} from 'react-icons/tb'
 
 const Navbar = () => {
+    const[active, setActive] = useState('navBar')
+    //Function to toggle navBar
+    const showNav = () =>{
+        setActive('navBar activeNavbar')
+    }
+
+    //Function to remove navBar
+    const removeNavbar = () =>{
+        setActive('navBar')
+    }
   return (
     <section className='navBarSection'>
         <header className='header flex'>
@@ -14,7 +24,7 @@ const Navbar = () => {
                 </a>
             </div>
 
-            <div className='navBar'>
+            <div className={active}>
                 <ul className='navLists flex'>
                     <li className='navItem'>
                         <a href='#' className='navLink'>Home</a>
@@ -49,12 +59,14 @@ const Navbar = () => {
                     </button>
                 </ul>
 
-                <div className='closeNavbar'>
+                <div onClick={removeNavbar}
+                className='closeNavbar'>
                     <AiFillCloseCircle className='icon' />
                 </div>
             </div>
 
-            <div className='toggleNavbar'>
+            <div onClick={showNav}
+            className='toggleNavbar'>
                 <TbGridDots className='icon' />
             </div>
         </header>
